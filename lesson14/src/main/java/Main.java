b0d62041339670a2c8b489edba71933a49ae7deb
Vyacheslav Kovalenko <vjacheslavkovalenko@gmail.com>
2024-09-17 18:49:32 +0300

    Commit N2. For check.

--- lesson14/src/main/java/Main.java
+++ lesson14/src/main/java/Main.java
@@ -1,34 +1,24 @@
-
-//Для решения задачи, где необходимо взять n подряд идущих чисел из стрима, определить количество четных и нечетных чисел, а также посчитать их сумму, мы можем воспользоваться Java Stream API. Результат будет представлен в виде Map<Type, Integer>, где Type будет представлять тип числа (четное или нечетное).
-
-//Описание программы
-//Импорт необходимых классов:
-//HashMap и Map для хранения результатов.
-//IntStream для работы с потоками чисел.
-//Основной метод main:
-//Определяется количество подряд идущих чисел n.
-//Используется IntStream.rangeClosed(1, n) для получения n подряд идущих чисел, начиная с 1.
-//Применяется метод reduce для подсчета четных и нечетных чисел, а также их сумм:
-//В качестве аккумулятора используется HashMap, в который добавляются значения для четных и нечетных чисел.
-//Внутри лямбда-выражения проверяется, является ли число четным или нечетным, и соответствующие значения обновляются.
-//Второй параметр в reduce используется для объединения результатов, если поток разбивается на части (что не требуется в данном случае, но полезно для параллельной обработки).
-//Вывод результата:
-//Результаты выводятся на консоль, показывая количество и сумму четных и нечетных чисел.
-
+/*
+ * Домашнее задание.
+ ** Задание 14
+ *** Есть стрим из чисел.
+ *** Необходимо взять n-количество подряд идущих чисел и определить сколько в них четных, а сколько нечетных.
+ *** Так же посчитать сумму четных и нечетных чисел.
+ *** Результат представить в виде map<Type, Integer>.
+ */
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-public class EvenOddCounter {
+public class Main{
     public static void main(String[] args) {
-        int n = 20; // Количество подряд идущих чисел
+        int n = 20;
 
-        // Получаем n подряд идущих чисел, начиная с 1
         Map<String, Integer> result = IntStream.rangeClosed(1, n)
-                .boxed() // Преобразуем IntStream в Stream<Integer>
+                .boxed()
                 .reduce(new HashMap<String, Integer>(), (map, number) -> {
-                    // Определяем, четное или нечетное
+
                     if (number % 2 == 0) {
                         map.put("evenCount", map.getOrDefault("evenCount", 0) + 1);
                         map.put("evenSum", map.getOrDefault("evenSum", 0) + number);
@@ -38,7 +28,6 @@ public class EvenOddCounter {
                     }
                     return map;
                 }, (map1, map2) -> {
-                    // Сливаем два результата
                     map1.put("evenCount", map1.getOrDefault("evenCount", 0) + map2.getOrDefault("evenCount", 0));
                     map1.put("evenSum", map1.getOrDefault("evenSum", 0) + map2.getOrDefault("evenSum", 0));
                     map1.put("oddCount", map1.getOrDefault("oddCount", 0) + map2.getOrDefault("oddCount", 0));
@@ -46,7 +35,6 @@ public class EvenOddCounter {
                     return map1;
                 });
 
-        // Вывод результата
         System.out.println("Результат:");
         System.out.println("Количество четных чисел: " + result.get("evenCount"));
         System.out.println("Сумма четных чисел: " + result.get("evenSum"));
