9a7a0bafb00f6bd02f536bc804c00069d7f7c0f2
Vyacheslav Kovalenko <vjacheslavkovalenko@gmail.com>
2024-09-17 18:31:49 +0300

    Commit N. For check.

--- lesson12/src/main/java/Exercise2.java
+++ lesson12/src/main/java/Exercise2.java
@@ -1,40 +1,37 @@
-//Задание 2
-//В этом задании мы создадим метод, который принимает массив строк и возвращает Map<String, Boolean>, где ключами являются строки, а значениями — true, если строка встречается 2 или более раз.
-
-
-//Описание программы
-//Метод wordMultiple:
-//-Создает два Map: один для подсчета вхождений строк, другой — для результата.
-//-Подсчитывает количество вхождений каждой строки в массиве.
-//-Заполняет результирующий Map, устанавливая значение true, если строка встречается 2 или более раз.
-//Метод main:
-//-Тестирует метод wordMultiple с различными массивами строк и выводит результаты.
+/*
+ * Домашнее задание.
+ ** Задание 12.2
+ *** На вход поступает массив строк, верните Map<String, Boolean>, где каждая отдельная строка является ключом,
+ *** и ее значение равно true, если эта строка встречается в массиве 2 или более раз.
+ *** Пример:
+ **** wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}
+ **** wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}
+ **** wordMultiple(["c", "c", "c", "c"]) → {"c": true}
+ */
 
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class WordMultiple {
+public class Exercise2 {
     public static void main(String[] args) {
         String[] words1 = {"a", "b", "a", "c", "b"};
         String[] words2 = {"c", "b", "a"};
         String[] words3 = {"c", "c", "c", "c"};
 
-        System.out.println(wordMultiple(words1)); // {"a": true, "b": true, "c": false}
-        System.out.println(wordMultiple(words2)); // {"a": false, "b": false, "c": false}
-        System.out.println(wordMultiple(words3)); // {"c": true}
+        System.out.println(wordMultiple(words1));
+        System.out.println(wordMultiple(words2));
+        System.out.println(wordMultiple(words3));
     }
 
     public static Map<String, Boolean> wordMultiple(String[] words) {
         Map<String, Integer> wordCount = new HashMap<>();
         Map<String, Boolean> result = new HashMap<>();
 
-        // Подсчет количества вхождений каждой строки
         for (String word : words) {
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
 
-        // Заполнение результата
         for (String word : wordCount.keySet()) {
             result.put(word, wordCount.get(word) >= 2);
         }
